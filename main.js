ce7beefd6efe3c320a6b09b2bcdcaeed052071ce
PandasMagic <pandasmagic1@gmail.com>
2022-06-03 23:21:42 -0700

    v1.0.2

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const { app, BrowserWindow, session } = require('electron')
+const { app, BrowserWindow, clipboard, ipcMain } = require('electron')
 const path = require('path')
-const Store = require('electron-store')
+const shortcut = require('electron-localshortcut')
 const { autoUpdater } = require("electron-updater")
-
-const store = new Store();
+require("v8-compile-cache");
 
 function settings() {
   app.commandLine.appendSwitch("disable-web-security", "true")
@@ -15,36 +14,57 @@ function settings() {
   app.commandLine.appendSwitch('use-angle', 'd3d9');
   app.commandLine.appendSwitch('enable-webgl2-compute-context');
   app.commandLine.appendSwitch("ignore-gpu-blacklist");
+  console.log("settings enabled")
 }
 
-const start = () => {
-  const mainWindow = new BrowserWindow({
+ipcMain.on('close', (event, arg) => {
+  app.quit();
+})
+
+app.on('ready', function () {
+  autoUpdater.checkForUpdatesAndNotify();
+
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     fullscreen: true,
     autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-
     }
   })
-  mainWindow.loadURL('https://ev.io/')
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.setTitle("Comp Client")
-    mainWindow.show()
-  })
-}
-
-app.on('ready', function () {
-  autoUpdater.checkForUpdatesAndNotify();
   settings();
-  start();
+  win.loadURL('https://ev.io/')
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+
+  shortcut.register(win, 'F11', () => {
+    win.setFullScreen(!win.isFullScreen());
+  });
+
+  shortcut.register(win, 'F5', () => {
+    win.loadURL("https://ev.io/");
+  });
+
+  shortcut.register(win, 'F6', () => {
+    win.loadURL(clipboard.readText());
+  });
+
+  shortcut.register(win, 'ALT+F4', () => {
+    app.quit();
+  });
+
+  shortcut.register(win, 'ESC', () => {
+    win.webContents.send('menu');
+  });
+
+  shortcut.register(win, 'F12', () => {
+    win.webContents.openDevTools()
+  });
 });
 
-autoUpdater.on('update-not-available', (info) => {
-
-})
 autoUpdater.on('update-downloaded', (info) => {
   autoUpdater.quitAndInstall();
 });
@@ -54,6 +74,9 @@ app.on("browser-window-created", (e, window) => {
   if (window.webContents.getURL() == "https://ev.io/") {
     window.close();
   }
+  ipcMain.on('close', () => {
+    app.quit();
+  })
 });
 
 app.on('window-all-closed', () => {
